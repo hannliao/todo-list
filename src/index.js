afde722182a909666e34ee4b1bfc00022bf2b936
Hannah Liao <128202377+hannliao@users.noreply.github.com>
2024-05-16 17:26:50 -0600

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./style.css"
-import { renderSidebar, renderMain } from "./dom.js"
-import initializeProjects from "./storage.js";
-
-initializeProjects();
-renderSidebar();
-renderMain("all");
